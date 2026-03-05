cf78d833c9456eba51ebfe16d0c62a3f382ef018
SahasKoyada <sahaskoyada@gmail.com>
2026-03-05 14:09:55 -0500

    auto pls work

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -217,6 +217,9 @@ public class RobotContainer {
         autoChooser.addOption("Ballin",
             Ballin.create(turret, indexer, transfer));
         autoChooser.addOption("Preload and Depot", new PathPlannerAuto("PreloadDepotv2", false));
+        autoChooser.addOption("Preload", new PathPlannerAuto("CenterPreloadAuto", false));
+        autoChooser.addOption("Preload and Depot V2(use this one)", new PathPlannerAuto("CenterPreloadDepotAuto", false));
+
         
         
         //autoChooser.addOption("MECK) Align Right",

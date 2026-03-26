73b25c808032364367bf52c67d33253b1db5361d
SahasKoyada <sahaskoyada@gmail.com>
2026-03-26 06:48:56 -0400

    more TurretLock

--- src/main/java/frc/robot/commands/TurretLock.java
+++ src/main/java/frc/robot/commands/TurretLock.java
@@ -28,19 +28,73 @@ public class TurretLock extends Command {
   public TurretLock(Turret turret) {
     this.turret = turret;
     turretPID = new PIDController(turret.sbTURRET_P.getDouble(0.1), 0, turret.sbTURRET_D.getDouble(0));
+    turretPID.setTolerance(1.0);
     addRequirements(turret);
   }
+  private Double getTagTx(int wantedId) {
+    LimelightHelpers.RawFiducial[] fiducials = LimelightHelpers.getRawFiducials(LimelightFrontName);
+    for (LimelightHelpers.RawFiducial f : fiducials) {
+      if (f.id == wantedId) {
+        return f.txnc;
+      }
+    }
+    return null;
+  }
 
+  private Double getMidpointTx(int id1, int id2) {
+    Double tx1 = getTagTx(id1);
+    Double tx2 = getTagTx(id2);
+
+    if (tx1 == null || tx2 == null) {
+      return null;
+    }
+
+    return (tx1 + tx2) / 2.0;
+  }
+
+  private double getSingleTagOffset(int id) {
+    switch (id) {
+      case 24:
+      case 8:
+        return turret.HubSideOffset.getDouble(1);
+
+      case 25:
+      case 9:
+        return turret.HubMiddleOffset.getDouble(0.5);
+
+      case 26:
+      case 10:
+        return 0.0;
+
+      case 27:
+      case 11:
+        return -turret.HubSideOffset.getDouble(1);
+
+      case 23:
+      case 7:
+        return turret.trenchOffset.getDouble(5);
+
+      case 28:
+      case 12:
+        return -turret.trenchOffset.getDouble(5);
+
+      default:
+        return 0.0;
+    }
+  }
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    //turretPID.reset();
+    turretPID.setP(turret.sbTURRET_P.getDouble(0.1));
+    turretPID.setD(turret.sbTURRET_D.getDouble(0.0));
     turretPID.reset();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double offset = 0;
+    /*double offset = 0;
     switch ((int)LimelightHelpers.getFiducialID(LimelightFrontName)) {
       case 24:
         offset = turret.HubSideOffset.getDouble(1);//change ts
@@ -94,6 +148,58 @@ public class TurretLock extends Command {
     } else {
       turret.stopTurret();
     }
+  }*/
+  if (!LimelightHelpers.getTV(LimelightFrontName)) {
+    turretPID.reset();
+    turret.stopTurret();
+    return;
+    }
+
+    Double desiredTx = null;
+
+    Double mid25_26 = getMidpointTx(25, 26);
+    Double mid26_27 = getMidpointTx(26, 27);
+    Double mid24_25 = getMidpointTx(24, 25);
+
+    Double mid9_10 = getMidpointTx(9, 10);
+    Double mid10_11 = getMidpointTx(10, 11);
+    Double mid8_9 = getMidpointTx(8, 9);
+
+    if (mid25_26 != null) {
+      desiredTx = mid25_26;
+    } else if (mid26_27 != null) {
+      desiredTx = mid26_27;
+    } else if (mid24_25 != null) {
+      desiredTx = mid24_25;
+    } else if (mid9_10 != null) {
+      desiredTx = mid9_10;
+    } else if (mid10_11 != null) {
+      desiredTx = mid10_11;
+    } else if (mid8_9 != null) {
+      desiredTx = mid8_9;
+    } else {
+      int id = (int) LimelightHelpers.getFiducialID(LimelightFrontName);
+      double tx = LimelightHelpers.getTX(LimelightFrontName);
+      double offset = getSingleTagOffset(id);
+      desiredTx = tx - offset;
+    }
+
+    double output = turretPID.calculate(desiredTx);
+    output = MathUtil.clamp(output, -0.20, 0.20);
+
+    System.out.println("desiredTx=" + desiredTx + " output=" + output);
+
+    if (Math.abs(desiredTx) < 1.0) {
+      turretPID.reset();
+      turret.stopTurret();
+      return;
+    }
+    if (Math.abs(output) < 0.02) {
+      turretPID.reset();
+      turret.stopTurret();
+      return;
+    }
+    turret.setDutyCycle(output);
   }
 
   // Called once the command ends or is interrupted.

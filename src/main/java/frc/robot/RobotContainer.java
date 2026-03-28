f193068efcdcb27a4490b2d038e4394a1f952375
Saurya <coolsaurya2010@gmail.com>
2026-03-28 08:57:38 -0400

    stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -428,7 +428,7 @@ public class RobotContainer {
             operator.rightTrigger().whileTrue(turret.runShooterRPM(turret::getDashboardTopRpm, turret::getDashboardBottomRpm));
             operator.back().onTrue(Commands.runOnce(() -> turret.zeroTurret(), turret));
             //run shooter at rpm determined by vision, run indexer when shooter is right speed
-            operator.leftTrigger().whileTrue(new AutoShoot(indexer, turret, transfer));
+            operator.leftTrigger().whileTrue(new AutoShoot(turret));
             
          
 

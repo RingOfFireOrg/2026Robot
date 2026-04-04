2a0d7782e8da86cf378d9447abdf37c0548128c5
SahasKoyada <sahaskoyada@gmail.com>
2026-04-04 13:58:15 -0400

    lowered current limits

--- src/main/java/frc/robot/subsystems/Transfer/Transfer.java
+++ src/main/java/frc/robot/subsystems/Transfer/Transfer.java
@@ -35,7 +35,7 @@ public class Transfer extends SubsystemBase {
   public Transfer() {
     SparkMaxConfig config = new SparkMaxConfig();
     config.idleMode(IdleMode.kCoast);
-    config.smartCurrentLimit(40);
+    config.smartCurrentLimit(30);
 
     motor.configure(
         config,
